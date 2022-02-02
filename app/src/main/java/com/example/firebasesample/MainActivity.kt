2236f9b111b1fed9604b6811c4df0cfbc0bfe65b
shuhei.kiyosawa <shuhei.kiyosawa@karadanote.jp>
2022-02-02 16:43:16 +0900

    匿名認証してみる

--- app/src/main/java/com/example/firebasesample/MainActivity.kt
+++ app/src/main/java/com/example/firebasesample/MainActivity.kt
@@ -2,10 +2,122 @@ package com.example.firebasesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // [END initialize_auth]
     }
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        signInAnonymously()
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+    // [END on_start_check_user]
+
+    private fun signInAnonymously() {
+        // [START signin_anonymously]
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInAnonymously:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInAnonymously:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END signin_anonymously]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    companion object {
+        private const val TAG = "AnonymousAuth"
+    }
+//    // [START declare_auth]
+//    private lateinit var auth: FirebaseAuth
+//    // [END declare_auth]
+//
+//    private var customToken: String? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        FirebaseApp.initializeApp(this)
+//        // [START initialize_auth]
+//        // Initialize Firebase Auth
+//        auth = Firebase.auth
+//        // [END initialize_auth]
+//    }
+//
+//    // [START on_start_check_user]
+//    public override fun onStart() {
+//        super.onStart()
+//
+//        startSignIn()
+//
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        updateUI(currentUser)
+//    }
+//    // [END on_start_check_user]
+//
+//    private fun startSignIn() {
+//        // Initiate sign in with custom token
+//        // [START sign_in_custom]
+//        customToken?.let {
+//            auth.signInWithCustomToken(it)
+//                .addOnCompleteListener(this) { task ->
+//                    if (task.isSuccessful) {
+//                        // Sign in success, update UI with the signed-in user's information
+//                        Log.d(TAG, "signInWithCustomToken:success")
+//                        val user = auth.currentUser
+//                        updateUI(user)
+//                    } else {
+//                        // If sign in fails, display a message to the user.
+//                        Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+//                        Toast.makeText(baseContext, "Authentication failed.",
+//                            Toast.LENGTH_SHORT).show()
+//                        updateUI(null)
+//                    }
+//                }
+//        }
+//        // [END sign_in_custom]
+//    }
+//
+//    private fun updateUI(user: FirebaseUser?) {
+//
+//    }
+//
+//    companion object {
+//        private const val TAG = "CustomAuthActivity"
+//    }
+
 }
